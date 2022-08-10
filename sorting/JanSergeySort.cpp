e2026ef9f68c4a0c3af2c4e136a5a9fd04273972
srad <ssedighi@posteo.de>
2022-08-10 14:22:58 +0000

    Migrating kernel.

--- sorting/JanSergeySort.cpp
+++ sorting/JanSergeySort.cpp
@@ -3,13 +3,14 @@
 #include "../datastructures.h"
 #include "../common.h"
 
-__device__ void prescan(int* countAndPrefixes);
-
 XPU_IMAGE(JanSergeySortKernel);
 
 struct JanSergeySortSmem {
-    int* countAndPrefixes;
+    int countAndPrefixes[experimental::channelCount];
+    int temp[experimental::channelCount];
 };
+
+XPU_D void prescan(JanSergeySortSmem& smem);
 
 XPU_KERNEL(JanSergeySort, JanSergeySortSmem, const int n, const experimental::CbmStsDigi* digis, const int* startIndex, const int* endIndex, experimental::CbmStsDigi* output) {
     const int bucketIdx = xpu::block_idx::x();
@@ -17,78 +18,76 @@ XPU_KERNEL(JanSergeySort, JanSergeySortSmem, const int n, const experimental::Cb
     const int bucketEndIdx = endIndex[bucketIdx];
     const int threadStart = bucketStartIdx + xpu::thread_idx::x();
 
-    __shared__ int countAndPrefixes[experimental::channelCount];
+    //__shared__ int countAndPrefixes[experimental::channelCount];
 
     // 1. Init all channel counters to zero: O(channelCount)
     for (int i = xpu::thread_idx::x(); i < experimental::channelCount; i += xpu::block_dim::x()) {
-        countAndPrefixes[i] = 0;
+        smem.countAndPrefixes[i] = 0;
     }
 
-    __syncthreads();
+    xpu::barrier();
 
     // 2. Count channels: O(n + channelCount)
     for (int i = threadStart; i <= bucketEndIdx && i < n; i += xpu::block_dim::x()) {
-        xpu::atomic_add_block(&countAndPrefixes[digis[i].channel], 1);
+        xpu::atomic_add_block(&smem.countAndPrefixes[digis[i].channel], 1);
     }
 
-    __syncthreads();
+    xpu::barrier();
 
     // 3. Prefix sum computation, TODO: replace by cub: https://nvlabs.github.io/cub/structcub_1_1_device_scan.html
-    prescan(countAndPrefixes);
+    prescan(smem);
 
-    __syncthreads();
+    xpu::barrier();
 
     // 4. Final sorting, place the elements in the correct position within the global output array: O(n)
     for (int i = bucketStartIdx + xpu::thread_idx::x(); i <= bucketEndIdx && i < n; i += xpu::block_dim::x()) {
-        output[bucketStartIdx + xpu::atomic_add_block(&countAndPrefixes[digis[i].channel], 1)] = digis[i];
+        output[bucketStartIdx + xpu::atomic_add_block(&smem.countAndPrefixes[digis[i].channel], 1)] = digis[i];
     }
 }
 
-__device__ void prescan(int* countAndPrefixes) {
-    __shared__ int temp[experimental::channelCount];// allocated on invocation
-
+XPU_D void prescan(JanSergeySortSmem& smem) {
     const int n = experimental::channelCount;
     const int countOffset = 0;
 
-    int thid = threadIdx.x;
+    int thid = xpu::thread_idx::x();
     int offset = 1;
 
     // load input into shared memory
-    temp[2 * thid] = countAndPrefixes[countOffset + 2 * thid];
-    temp[2 * thid + 1] = countAndPrefixes[countOffset + 2 * thid + 1];
+    smem.temp[2 * thid] = smem.countAndPrefixes[countOffset + 2 * thid];
+    smem.temp[2 * thid + 1] = smem.countAndPrefixes[countOffset + 2 * thid + 1];
 
     // build sum in place up the tree
     for (int d = n >> 1; d > 0; d >>= 1) {
-        __syncthreads();
+        xpu::barrier();
         if (thid < d) {
             int ai = offset * (2 * thid + 1) - 1;
             int bi = offset * (2 * thid + 2) - 1;
-            temp[bi] += temp[ai];
+            smem.temp[bi] += smem.temp[ai];
         }
         offset *= 2;
     }
 
-     if (thid == 0) { temp[n - 1] = 0; } // clear the last element
+     if (thid == 0) { smem.temp[n - 1] = 0; } // clear the last element
 
      // traverse down tree & build scan
      for (int d = 1; d < n; d *= 2) {
          offset >>= 1;
 
-         __syncthreads();
+         xpu::barrier();
 
          if (thid < d) {
              int ai = offset * (2 * thid + 1) - 1;
              int bi = offset * (2 * thid + 2) - 1;
-             float t = temp[ai];
-             temp[ai] = temp[bi];
-             temp[bi] += t;
+             float t = smem.temp[ai];
+             smem.temp[ai] = smem.temp[bi];
+             smem.temp[bi] += t;
          }
      }
 
-     __syncthreads();
+     xpu::barrier();
 
-     countAndPrefixes[countOffset + 2 * thid] = temp[2 * thid]; // write results to device memory
-     countAndPrefixes[countOffset + 2 * thid + 1] = temp[2 * thid + 1];
+     smem.countAndPrefixes[countOffset + 2 * thid] = smem.temp[2 * thid]; // write results to device memory
+     smem.countAndPrefixes[countOffset + 2 * thid + 1] = smem.temp[2 * thid + 1];
 }
 
 /*
