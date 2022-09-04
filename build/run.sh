2e7a690a8788bde761ec30b5155e1ae6a6288cba
srad <ssedighi@posteo.de>
2022-09-04 19:28:36 +0000

    Fix str::sort benchmark. Additional simple JanSergeySort implementation as reference. * std::sort buckets, applying sorting to bucket and not to entire sequence, and doing it in parallel. Still seems to contain bug, see output. * JanSergeySortSimpleSum uses one block per bucket and the linear exclusive sum computation with one loop (O(1) runtime). For reference. * Using DEBUG_SORT symbol, for debugging the sorting result, it will include the digi "address" in the written output file. The real benchmark should not use this symbol and will remove this property from the struct.

--- build/run.sh
+++ build/run.sh
@@ -1,2 +1,3 @@
+rm output/*
 make -j64
-XPU_DEVICE=hip0 LD_LIBRARY_PATH=.:lib/xpu ./stsdigisort -i ../data/digis_2022-08-23_13-05-03_ev200_auau_25gev_centr_1_1_0.csv -r 1 -w -c
+XPU_DEVICE=cuda0 LD_LIBRARY_PATH=.:lib/xpu ./stsdigisort -i ../data/digis_2022-08-23_13-05-03_ev200_auau_25gev_centr_1_1_0.csv -r 1 -w -c
